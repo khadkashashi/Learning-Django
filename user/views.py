7814d862d8baf0a0a3ce4d56e6ff241da4bf306b
shashi khadka <shashikhadka84@gmail.com>
2026-06-18 16:58:15 +0545

    user register

--- user/views.py
+++ user/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-from user.forms import RegisterForm
-
+from django.shortcuts import render, redirect
+from user.forms import RegisterFrom
 # Create your views here.
 def register(request):
-    form =RegisterForm
-    context={
-        'form':form
+    form = RegisterFrom()
+    if request.method == "POST":
+        form = RegisterFrom(data=request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(request.POST['password'])
+            user.save()
+            return redirect('/admin')
+    context = {
+        "form":form
     }
-    return render(request,'user/register.html',context)
+    return render(request, 'user/register.html',context)
